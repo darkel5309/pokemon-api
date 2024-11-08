fe940eecfeae3007ecc8f53fa9a97234c9c72e60
Angel <angeldzrz3103@gmail.com>
2024-11-08 10:30:14 +0100

    primer commit

---
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../../models/pokemon';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrl: './pokemon-list.component.css'
+})
+export class PokemonListComponent implements OnInit{
+
+  public pokemons: Pokemon[] = [];
+
+  
+  constructor(
+    private pokemonService: PokemonService
+  ){}
+
+  ngOnInit(): void {
+    this.pokemonService.getPokemon().subscribe(
+      (data:any) => {
+        let (count, next, previous, results) = data;
+        this.pokemons = results;
+      }
+    );
+  }
+}
---
+++ src/app/models/pokemon.ts
@@ -0,0 +1,6 @@
+export class Pokemon {
+    constructor(
+        name: string,
+        url: string
+    ){}
+}
